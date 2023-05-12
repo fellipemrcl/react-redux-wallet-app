01e75985e5acd2eb734ef5ea9805ae823fd75aaf
Fellipe Marcel <fmrclms@gmail.com>
2023-05-12 17:32:08 -0300

    feat: implemented login page and its logic

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,86 @@
+import PropTypes from 'prop-types';
 import React from 'react';
+import { connect } from 'react-redux';
+import { saveEmail } from '../redux/actions';
 
 class Login extends React.Component {
+  state = {
+    email: '',
+    password: '',
+  };
+
+  handleChange = ({ target }) => {
+    const { name, value } = target;
+    this.setState({ [name]: value });
+  };
+
+  validateEmail = (email) => {
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    return emailRegex.test(email);
+  };
+
+  handleClick = () => {
+    const { email } = this.state;
+    const { dispatch, history } = this.props;
+    dispatch(saveEmail(email));
+    history.push('/carteira');
+  };
+
   render() {
-    return <div>Login</div>;
+    const { email, password } = this.state;
+    const minPassLeng = 6;
+    const isPasswordValid = password.length >= minPassLeng;
+    const isEmailValid = this.validateEmail(email);
+    return (
+      <div>
+        <h1>Login</h1>
+        <label htmlFor="emailInput">
+          Email:
+          <input
+            type="email"
+            id="emailInput"
+            data-testid="email-input"
+            onChange={ this.handleChange }
+            name="email"
+            value={ email }
+          />
+        </label>
+        <label htmlFor="passwordInput">
+          Senha:
+          <input
+            type="password"
+            id="passwordInput "
+            data-testid="password-input"
+            name="password"
+            onChange={ this.handleChange }
+            value={ password }
+          />
+        </label>
+        <button
+          type="button"
+          disabled={ !isPasswordValid || !isEmailValid }
+          onClick={ this.handleClick }
+        >
+          Entrar
+
+        </button>
+      </div>
+    );
   }
 }
 
-export default Login;
+Login.propTypes = {
+  dispatch: PropTypes.func,
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }),
+};
+
+Login.defaultProps = {
+  dispatch: () => {},
+  history: {
+    push: () => {},
+  },
+};
+
+export default connect()(Login);
--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,1 +1,14 @@
-// Coloque aqui suas actions.
+export const actionTypes = {
+  SAVE_EMAIL: 'SAVE_EMAIL',
+  GET_GURRENCIES: 'GET_GURRENCIES',
+};
+
+export const saveEmail = (email) => ({
+  type: actionTypes.SAVE_EMAIL,
+  payload: email,
+});
+
+export const getGurrencies = (currencies) => ({
+  type: actionTypes.GET_GURRENCIES,
+  payload: currencies,
+});
